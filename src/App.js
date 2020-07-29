ea47798486d69546fe6c24edfe7c613b058065de
Jonathan Portillo <jonathanportillo@MacBook-Pro.home>
2020-07-28 23:50:58 -0400

    added components and introduced api to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Body from "./Components/Body";
+import Header from "./Components/Header";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  const effectFn = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=8pa2gNvoS7XiS65lfXRrzERWT0aOWWRtcTuWV129"
+      )
+      .then((response) => {
+        console.log("success", response);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(effectFn);
+
   return (
     <div className="App">
+      <Header />
+      <Body />
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
---
+++ src/Components/Body.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Body = () => {
+  return <div>This is le Body</div>;
+};
+
+export default Body;
